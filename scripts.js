574f7f7c63c50d7f965a0a5e662b963633116093
AndreiTotoro <andreitotoro@gmail.com>
2020-11-10 20:38:59 +0200

    Version 1.0

---
+++ scripts.js
@@ -0,0 +1,42 @@
+const playerFactory = (playerName, marker) => {
+    return {playerName, marker}
+}
+
+const squareCreator = (sqNr, sqControl = "free") => {
+    return {sqNr, sqControl}
+}
+
+const gameManagerCreator = (score1 = 0, score2 = 0, round = 1) => {
+    return {score1, score2, round}
+}
+
+
+const player1 = playerFactory(name, marker)
+
+const player2 = playerFactory(name, marker)
+
+const gameBoard = [
+    squareCreator(1),
+    squareCreator(2),
+    squareCreator(3),
+    squareCreator(4),
+    squareCreator(5),
+    squareCreator(6),
+    squareCreator(7),
+    squareCreator(8),
+    squareCreator(9)
+]
+
+const gameManager = gameManagerCreator();
+
+const board = document.querySelector('#board')
+
+gameBoard.forEach(obj => {
+    
+}
+
+
+
+
+
+
